469a2e41db0ae6781caa33a93fa1f689954862df
JoErnesto <jernesto.calvillo@gmail.com>
2022-10-17 20:37:18 -0700

    modificaciones

--- rfid-args-mysql.py
+++ rfid-args-mysql.py
@@ -1,7 +1,7 @@
 #Bibliotecas
 from time import sleep
 import sys
-from mfrc511 import SimpleMFRC522
+from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
 import mysql.connector
 
@@ -9,7 +9,7 @@ import mysql.connector
 reader = SimpleMFRC522()
 
 #Conexión
-cnx= mysql.connector.connect(user='joseernestolan', password='1590', host='192.168.1.93', database='codigoIoT')
+cnx= mysql.connector.connect(user='joseernestolan', password='1234', host='192.168.1.93', database='codigoIoT')
 
 #Cursor
 cursor = cnx.cursor()
@@ -18,7 +18,9 @@ cursor = cnx.cursor()
 try:
     #Lectura unica
     id, text = reader.read()
-    print("ID: %S\nText: %S" % (id,text))
+    print("ID: %s\nText: %s" % (id,text))
+    strrr = text.split(",")
+    print(strrr)
     sleep(1)
 
 except KeyboardInterrupt:
