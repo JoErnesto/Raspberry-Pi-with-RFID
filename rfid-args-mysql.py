3e489c1fce81b50f8f7a49ada6075aa966f69439
JoErnesto <jernesto.calvillo@gmail.com>
2022-10-17 21:17:32 -0700

    MOdificacion
    Ultima modificacion para que se registre en mysql.

--- rfid-args-mysql.py
+++ rfid-args-mysql.py
@@ -4,6 +4,12 @@ import sys
 from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
 import mysql.connector
+import argparse
+
+#Parser
+parser = argparse.ArgumentParser()
+parser .add_argument("status")
+args = parser.parse_args()
 
 #Iniciar el sensor
 reader = SimpleMFRC522()
@@ -18,10 +24,25 @@ cursor = cnx.cursor()
 try:
     #Lectura unica
     id, text = reader.read()
-    print("ID: %s\nText: %s" % (id,text))
+    #print("ID: %s\nText: %s" % (id,text))
     strrr = text.split(",")
-    print(strrr)
+    #print(strrr)
+    #print(strrr[0])
     sleep(1)
+    query_insert = "INSERT INTO RFID (nombre,texto,rfid) VALUES ('" + strrr[0] + "','" + args.status + "'," + str (id) +");"
+    print(query_insert)
+
+    #Ejecutar cursor
+    cursor.execute(query_insert)
+    #ASegurrse de realizar la operacion en BD
+    cnx.commit()
+    print("Query ok")
+    sleep(1)
+
+    #Cerrar
+    cursor.close()
+    cnx.close()
+    GPIO.cleanup()
 
 except KeyboardInterrupt:
     #Cerrar
